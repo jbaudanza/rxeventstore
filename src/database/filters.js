181b0a31be11a73f96728aab4cbbcb2aa63a559b
Jonathan Baudanza <jon@jonb.org>
2016-11-01 15:28:08 -0700

    Filters.toFunction

--- src/database/filters.js
+++ src/database/filters.js
@@ -40,3 +40,86 @@ export function toSQL(filters) {
 
   return [conditions.join(' AND '), values];
 }
+
+
+// This is from https://github.com/joliss/js-string-escape
+function escapeJsStringLiteral(string) {
+  return ('' + string).replace(/["'\\\n\r\u2028\u2029]/g, function (character) {
+    // Escape all characters not included in SingleStringCharacters and
+    // DoubleStringCharacters on
+    // http://www.ecma-international.org/ecma-262/5.1/#sec-7.8.4
+    switch (character) {
+      case '"':
+      case "'":
+      case '\\':
+        return '\\' + character
+      // Four possible LineTerminator characters need to be escaped:
+      case '\n':
+        return '\\n'
+      case '\r':
+        return '\\r'
+      case '\u2028':
+        return '\\u2028'
+      case '\u2029':
+        return '\\u2029'
+    }
+  });
+}
+
+function escapeJsLiteral(literal) {
+  if (typeof literal === 'string') {
+    return '"' + escapeJsStringLiteral(literal) + '"';
+  } else {
+    return String(literal);
+  }
+}
+
+export function toFunction(filters) {
+  const filterKeys = Object.keys(filters);
+  let conditions;
+
+  if (filterKeys.length === 0) {
+    conditions = ['true'];
+  } else {
+    conditions = filterKeys.map(function(attributeName) {
+      let value = filters[attributeName];
+      let operator = '===';
+
+      if (typeof value === 'object') {
+        const keys = Object.keys(value);
+        if (keys.length === 1) {
+          switch (keys[0]) {
+            case '$gt':
+              value = value[keys[0]];
+              operator = '>';
+              break;
+            case '$lt':
+              value = value[keys[0]];
+              operator = '<';
+              break;
+            case '$eq':
+              value = value[keys[0]];
+              operator = '===';
+              break;
+          }
+        }
+      }
+
+      const lhs = 'o["' + escapeJsStringLiteral(attributeName) + '"]';
+
+      if (Array.isArray(value)) {
+        return '(' + value.map(function(option) {
+          return lhs + '===' + escapeJsLiteral(option);
+        }).join('||') + ')';
+      } else {
+        return lhs + operator + escapeJsLiteral(value);
+      }
+    });
+  }
+
+  const js = '(function(o){return '
+    + conditions.join('&&')
+    + ';})';
+
+  return eval(js);
+}
--- test/filters_spec.js
+++ test/filters_spec.js
@@ -2,16 +2,18 @@ import assert from 'assert';
 
 import * as filters from '../lib/database/filters';
 
+const testFilters = {
+  foo: true,
+  bar: false,
+  fish: {$eq: 'salmon'},
+  amount: {$lt: 1.99},
+  count: {$gt: 55},
+  color: ['red', 'blue']
+};
+
 describe("filters.toSQL", () => {
   it('should render a valid WHERE clause', () => {
-    const result = filters.toSQL({
-      foo: true,
-      bar: false,
-      fish: {$eq: 'salmon'},
-      amount: {$lt: 1.99},
-      count: {$gt: 55},
-      color: ['red', 'blue']
-    });
+    const result = filters.toSQL(testFilters);
 
     assert.deepEqual(result, 
       [
@@ -19,5 +21,41 @@ describe("filters.toSQL", () => {
         [ true, false, 'salmon', 1.99, 55, ['red', 'blue'] ]
       ]
     );
+  });
+});
+
+describe('filters.toFunction', () => {
+  it('should filter properly', () => {
+    const fn = filters.toFunction(testFilters);
+
+    const positive = {
+      foo: true,
+      bar: false,
+      fish: 'salmon',
+      amount: 1.00,
+      count: 60,
+      color: 'red'
+    };
+
+    assert(fn(positive));
+
+    function negative(obj) {
+      assert(!fn(Object.assign({}, positive, obj)));
+    }
+
+    // Negative tests
+    negative({foo: false});
+    negative({bar: true});
+    negative({fish: 'tuna'});
+    negative({amount: 5.00});
+    negative({count: 25});
+    negative({color: 'orange'});
+  });
+  it('should work with an empty set of filters', () => {
+    const fn = filters.toFunction({});
+    assert(fn(1));
+    assert(fn('hello'));
+    assert(fn(null));
+    assert(fn());
   });
 });
